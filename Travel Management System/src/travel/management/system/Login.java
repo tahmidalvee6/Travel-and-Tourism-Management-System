84c2d2a052de6871bf2b130d2c25c177fb9025e1
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2025-07-07 16:50:17 +0600

    Update Login.java

--- Travel Management System/src/travel/management/system/Login.java
+++ Travel Management System/src/travel/management/system/Login.java
@@ -1,83 +1,108 @@
 package travel.management.system;
 
-
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
 import javax.swing.*;
 
-public class Login extends JFrame {
-    JTextField usernameField;
-    JPasswordField passwordField;
-    JButton loginButton, signUpButton, forgotPasswordButton;
+public class Login extends JFrame implements ActionListener {
+
+    JButton login, signup, password;
+    JTextField tfusername, tfpassword;
 
     Login() {
+        setSize(900, 400);
+        setLocation(350, 200);
         setLayout(null);
-        getContentPane().setBackground(Color.WHITE);
-        setBounds(350, 200, 900, 400);
 
-        JPanel panel = new JPanel();
-        panel.setBackground(new Color(131, 193, 233));
-        panel.setBounds(0, 0, 400, 400);
-        panel.setLayout(null);
-        add(panel);
+        JPanel p1 = new JPanel();
+        p1.setBackground(new Color(131, 199, 233));
+        p1.setBounds(0, 0, 400, 400);
+        p1.setLayout(null);
+        add(p1);
 
-        // ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("icons/login(1).png"));
-        // Image image = icon.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
-        // JLabel label = new JLabel(new ImageIcon(image));
-        // label.setBounds(100, 120, 200, 200);
-        // panel.add(label);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("Travel/Management/System/icons/login.png"));
+        Image i2 = i1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(100, 120, 200, 200);
+        p1.add(image);
 
-        JLabel userLabel = new JLabel("Username");
-        userLabel.setBounds(450, 20, 100, 25);
-        add(userLabel);
+        JPanel p2 = new JPanel();
+        p2.setLayout(null);
+        p2.setBounds(400, 30, 450, 300);
+        add(p2);
 
-        usernameField = new JTextField();
-        usernameField.setBounds(550, 20, 150, 25);
-        add(usernameField);
+        JLabel lblusername = new JLabel("Username");
+        lblusername.setBounds(60, 20, 100, 25);
+        lblusername.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        p2.add(lblusername);
 
-        JLabel passLabel = new JLabel("Password");
-        passLabel.setBounds(450, 60, 100, 25);
-        add(passLabel);
+        tfusername = new JTextField();
+        tfusername.setBounds(60, 60, 300, 30);
+        tfusername.setBorder(BorderFactory.createEmptyBorder());
+        p2.add(tfusername);
 
-        passwordField = new JPasswordField();
-        passwordField.setBounds(550, 60, 150, 25);
-        add(passwordField);
+        JLabel lblpassword = new JLabel("Password");
+        lblpassword.setBounds(60, 110, 100, 25);
+        lblpassword.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        p2.add(lblpassword);
 
-        loginButton = new JButton("Login");
-        loginButton.setBounds(550, 110, 100, 25);
-        loginButton.setBackground(Color.BLACK);
-        loginButton.setForeground(Color.WHITE);
-        
-        add(loginButton);
+        tfpassword = new JTextField();
+        tfpassword.setBounds(60, 150, 300, 30);
+        tfpassword.setBorder(BorderFactory.createEmptyBorder());
+        p2.add(tfpassword);
 
-        signUpButton = new JButton("Sign Up");
-        signUpButton.setBounds(660, 110, 100, 25);
-        signUpButton.setBackground(Color.BLACK);
-        signUpButton.setForeground(Color.WHITE);
-        
-        add(signUpButton);
+        login = new JButton("Login");
+        login.setBounds(60, 200, 130, 30);
+        login.setBackground(new Color(133, 193, 233));
+        login.setForeground(Color.WHITE);
+        login.setBorder(BorderFactory.createEmptyBorder());
+        login.addActionListener(this);
+        p2.add(login);
 
-        forgotPasswordButton = new JButton("Forgot Password");
-        forgotPasswordButton.setBounds(550, 150, 210, 25);
-        forgotPasswordButton.setBackground(Color.BLACK);
-        forgotPasswordButton.setForeground(Color.WHITE);
-        
-        add(forgotPasswordButton);
+        signup = new JButton("Signup");
+        signup.setBounds(230, 200, 130, 30);
+        signup.setBackground(new Color(133, 193, 233));
+        signup.setForeground(Color.WHITE);
+        signup.setBorder(BorderFactory.createEmptyBorder());
+        signup.addActionListener(this);
+        p2.add(signup);
 
+        password = new JButton("Forget Password");
+        password.setBounds(130, 250, 130, 30);
+        password.setBackground(new Color(133, 193, 233));
+        password.setForeground(Color.WHITE);
+        password.setBorder(BorderFactory.createEmptyBorder());
+        password.addActionListener(this);
+        p2.add(password);
 
         setVisible(true);
     }
 
     public void actionPerformed(ActionEvent ae) {
-        if (ae.getSource() == loginButton) {
-            
-            
-        } else if (ae.getSource() == signUpButton) {
+        if (ae.getSource() == login) {
+            try {
+                String username = tfusername.getText();
+                String pass = tfpassword.getText();
+                String query = "select * from account where username = '" + username + "' AND password = '" + pass + "'";
+                DBConnection c = new DBConnection();
+                ResultSet rs = c.s.executeQuery(query);
+                if (rs.next()) {
+                    setVisible(false);
+                    new Loading(username);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Incorrect username or password");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (ae.getSource() == signup) {
             setVisible(false);
-            //new SignUp();
-        } else if (ae.getSource() == forgotPasswordButton) {
+            new Signup();
+        } else {
             setVisible(false);
-            //new ForgotPassword();
+            new ForgotPassword();
         }
     }
 
@@ -85,6 +110,15 @@ public class Login extends JFrame {
         new Login();
     }
 }
+
+
+
+
+
+
+
+
+
 
 
 

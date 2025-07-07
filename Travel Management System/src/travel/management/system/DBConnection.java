a2f008ae0436f8ffebd29ba42fc4f91b65473102
Tahmid Alvee <151709545+tahmidalvee6@users.noreply.github.com>
2025-07-07 16:51:42 +0600

    Add files via upload

---
+++ Travel Management System/src/travel/management/system/DBConnection.java
@@ -0,0 +1,21 @@
+package travel.management.system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DBConnection {
+
+    public Connection c;
+    public Statement s;
+
+    public DBConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/travel_and_tourism_management_system_db", "root", "");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
